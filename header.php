13bc308598d7f80b3b7a54b51b6c0c9c1e68aa93
manmohanmandhana <manmohanmandhana@users.noreply.github.com>
2014-08-26 11:58:09 +0530

    Update header.php
    deadline extended

--- header.php
+++ header.php
@@ -135,7 +135,7 @@ if(!isset($title)){
     <div class="modal-content">
       <div class="modal-header" >
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Abstract Submission ( DeadLine : 23 August,2014 )</h4>
+        <h4 class="modal-title" id="myModalLabel">Abstract Submission ( DeadLine(Extended) : 30 August,2014 )</h4>
       </div>
       
      <div class="modal-body">
