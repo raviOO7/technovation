aee58f9405ad4616b717467185ba986d674957c2
manmohanmandhana <manmohanmandhana@users.noreply.github.com>
2014-08-29 18:07:50 +0530

    Update TechGSR.php

--- TechGSR.php
+++ TechGSR.php
@@ -28,36 +28,19 @@
 
 <H3>Projects</H3>
 
-<b>Choice of technical projects to work on :</b><br/>
-(chose either these or management projects - not more than 3 from here)
+<b>Choice of Projects to work on :</b><br/>
+
  
 <ol>
-<li> Smokless chulah from mud - less wood consumption - use unburnt particles directly </li>
-<li> Warm and hygienic animal shelters by concentrating solar energy efficiently</li>
-<li> Find out an effective method to map the village with the exact stream lines</li>
-<li> Plan and design the drainage system  and watershed intervention opportunities</li>
-<li> Using the potential head, develop a drip irrigation system for the farms</li>
-<li> Rain harvesting stores made up necessarily of mud  </li>
-<li> A device  which based on the water level in well, intakes the chlorine content in the container to be added to the water  </li>
-<li> Quick and better quality manure preparation device via heat control and water content measure </li>
-<li> Vertical Garden,  plantation on cement bags, and more methods for vegetation plantation </li>
-<li> Nutrient contents and adulterants content study of the water in that area</li>
-<li> Nutrient contents and adulterants content study of the soil in that area</li>
-<li> Solar energy usage to effective lightning system using total internal reflection</li>
-<li> Refrigeration of food – Indigenous methods use</li>
-<li> Roof makeover as per the structural design – to minimize loading and prevent roof leakage </li>
-<li> Better slopes based design for houses to avoid any water clogging</li>
-<li> Low cost material analysis for the housing – from doors to furniture   </li>
-</ol>
-<br/>
-<b>Choice of management projects to work on :</b><br/>
-(chose either these or from technical projects - not more than 1 from here)
+<li> Develop the most efficient smokeless chulha with household materials only </li>
+<li> Create a pressure based drip irrigation technique (with no electricity or other means of energy) </li>
+<li> Well water purification using chlorine dissolution using only real time water level </li>
+<li> Catalytic natural manure making device </li>
+<li> Effective household lighting; use only one bulb to illuminate entire house  </li>
+<li> Vehicle mobility solution for medical emergency </li>
+<li> Model creation for handicraft based sustainable enterprise </li>
 
-<ol>
-<li> Making an effective business plan for Ayurvedic medicines selling by tribals in the village </li>
-<li> Making a B plan for opening a brick production enterprise for the tribals</li>
-<li> Making a B plan for opening a Tourism enterprise for the tribals</li>
- </ol>
+</ol>
 
 <br/>
 
