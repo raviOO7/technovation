359dc69d81418c8b815db9e4f6822d18be741c40
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-04-14 12:57:45 +0530

    Added Summer technovation

--- header.php
+++ header.php
@@ -135,7 +135,7 @@ if(!isset($title)){
 								    <div class="modal-content">
 								      <div class="modal-header" >
 								        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-								        <h4 class="modal-title" id="myModalLabel">Abstract Submission ( DeadLine : 19 August,2015 )</h4>
+								        <h4 class="modal-title" id="myModalLabel">Abstract Submission ( DeadLine : 19 april,2015 )</h4>
 								      </div>
 								      
 								     <div class="modal-body">
